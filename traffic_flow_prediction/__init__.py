229b6db6cd307e571c87c112ee66372204e6d47a
靳琦闻(Qiwen Jin) <850801464@qq.com>
2026-06-09 20:38:48 +0800

    Fix: Remove non-existent compute_metrics import
    - Remove import of compute_metrics which doesn't exist in evaluation.py
    - Keep only evaluate_predictions which is available
    - Update __all__ export list

--- traffic_flow_prediction/__init__.py
+++ traffic_flow_prediction/__init__.py
@@ -1,7 +1,7 @@
 """Traffic flow prediction package."""
 
 from .data_loader import TrafficDataset, load_traffic_dataset
-from .evaluation import compute_metrics, evaluate_predictions
+from .evaluation import evaluate_predictions
 from .models import build_models, train_and_predict
 from .preprocessing import preprocess_features
 
@@ -11,6 +11,5 @@ __all__ = [
     "preprocess_features",
     "build_models",
     "train_and_predict",
-    "compute_metrics",
     "evaluate_predictions",
 ]
